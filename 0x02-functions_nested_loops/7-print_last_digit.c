f32970acacfb54c2b9f67c7a5e664302ec7bc3af
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-15 07:56:40 -0700

    my commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,16 +6,16 @@
  */
 int print_last_digit(int n)
 {
-	int a;	
+int a;	
 
-	if (n < 0)
-		n = -n;
+if (n < 0)
+n = -n;
 
-	a = n % 10;		
+a = n % 10;		
 
-	if (a < 0)
-		a = -a;
-	_putchar(a + '0');
+if (a < 0)
+a = -a;
+_putchar(a + '0');
 	
 	return (a);		
 }
