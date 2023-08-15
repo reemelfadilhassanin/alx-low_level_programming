fe67ec7342c9e1446956a3ee1162c9bc78e44ebc
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-15 06:35:22 -0700

    my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include "main.h"
   
 /**
-         * main - print _putchar
-         * is written by reem
-         * Return: 0
-         */
-
+ * main - Entry point
+ * 
+ * Description: 'print_alphabet - print all alphabet in lowercase'
+ *
+ * 
+ * Return: Always 0 (Success)
+ */
 
 	void print_alphabet(void)
 	{
