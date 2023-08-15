f27fea2c1865ddbc45fdf01ccec8d705b8b3429f
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-15 09:38:12 -0700

    my commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,15 +4,13 @@
 	 */
 
 
-	#include <stdio.h>
-
-
-	/**
-	 * main - Lists all the natural numbers below 1024 (excluded)
-	 *        that are multiples of 3 or 5.
-	 *
-	 * Return: Always 0.
-	 */
+#include <stdio.h>
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
+ */
 	int main(void)
 	{
 		int i, sum = 0;
@@ -30,6 +28,3 @@
 
 		return (0);
 	}
-
-
-
