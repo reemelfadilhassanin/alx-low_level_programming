91db780ab10f4219ac0d22b08d2ceb9aa290884e
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-17 08:50:00 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * mul - multiply two numbers
+ * @a: firest input one
+ * @b: seconed input two
+ * Return: 0 or 1
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
