85fa22194e84e08ed590ada3fefa26e9ce1caf4d
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-21 13:16:01 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half -  prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: half of input
+ */
+void puts_half(char *str)
+{
+	int x, y, longi;
+
+	i = 0;
+
+	for (x = 0; str[x] != '\0'; x++)
+		i++;
+
+	y = (i / 2);
+
+	if ((i % 2) == 1)
+		y = ((i + 1) / 2);
+
+	for (x = y; str[x] != '\0'; x++)
+		_putchar(str[x]);
+	_putchar('\n');
+}
