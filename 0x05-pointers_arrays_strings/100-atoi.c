c84fc20de9cbe3b57f104bdb1144846d9a5154a4
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-21 13:53:47 -0700

    first commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: int converted 
+ *@s: string to be converted
+ *Return: int converted 
  */
 int _atoi(char *s)
 {
