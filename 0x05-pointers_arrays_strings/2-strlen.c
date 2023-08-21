08ffbff9bba9d6ba54464518bf10842a6caa378e
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-21 12:22:47 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+i#include "main.h"
+/**
+ * _strlen - returns the len of a string
+ * @s: string of input
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+
+int i;
+
+for (i = 0; *s != '\0'; s++)
+	i++;
+
+return (i);
+}
