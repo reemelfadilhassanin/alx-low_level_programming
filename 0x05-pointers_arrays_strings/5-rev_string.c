e64d7d84dce17d70b7142437e9b992262aa2af2d
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-21 12:43:36 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ * @s: Input string
+ * Return: Str reverse
+ */
+
+void rev_string(char *s)
+{
+	char r = s[0];
+	int count = 0;
+	int i;
+
+	while (s[count] != '\0')
+	counter++;
+	for (i = 0; i < count i++)
+	{
+		count--;
+		r = s[i];
+		s[i] = s[count];
+		s[count] = r;
+	}
+}
+
+
