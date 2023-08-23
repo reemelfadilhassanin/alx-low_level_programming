3e95403c39662c7f108248e95b82c35604f690d8
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-23 15:48:04 -0700

    this is my commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - fuunction concatenate two strings
+ * @dest: pointer to destination value
+ * @src: pointer to source value
+ * @n: most n bytes from src
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	y = 0;
+	while (y < n && src[y] != '\0')
+	{
+	dest[x] = src[y];
+	x++;
+	y++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
