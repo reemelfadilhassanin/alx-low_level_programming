2dae4886ee98f510211a355b8edf533504352ad8
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-23 15:38:51 -0700

    this is my commit

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,13 +1,11 @@
-#include "main.h"                                                                                                                  
-#include <unistd.h>                                                                                                                     
-/**                                                                                                                                     
- * _putchar - writes c to stdout                                                                                          
- * @c: The char to print                                                                                                           
- *                                                                                                                                      
- * Return: On success 1.                                                                                                                
- * On error, -1 is returned, and errno is set appropriately.                                                                            
- */                                                                                                                                     
-int _putchar(char c)                                                                                                                    
-{                                                                                                                                       
-        return (write(1, &c, 1));                                                                                                       
+#include <unistd.h>
+/**
+ * _putchar - write  c to stdout
+ * @c: To print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
 }
