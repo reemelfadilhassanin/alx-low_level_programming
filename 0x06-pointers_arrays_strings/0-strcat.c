bef583bd1912f9eac7152bd893767085ccdd3d6c
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-24 15:36:44 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int x;
-	int y;
+	int x = 0;
+	int y = 0;
 
-	x = 0;
 	while (dest[x] != '\0')
 	{
 		x++;
 	}
-	y = 0;
 	while (src[y] != '\0')
 	{
 		dest[x] = src[y];
