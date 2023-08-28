399e41fc25ccd5f3ceb4a494fb48d9296ccd9fe7
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-28 13:04:41 -0700

    smy commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to change
+ * @to: the value to change pointer
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
