7e150d7c696f74a037632acf0f33c9e48ded09c3
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-08-31 10:07:23 -0700

    my commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,32 @@
 #include"main.h"
+int sqrt1(int n, int i);
 /**
- *_sqrt_recursion - A function calc  square root of a number.
- *  @n: number to calclut squer
+ * sqrt - returns the natural square root of a number
+ * @n: number to calculate the square root of
  *
- *  Return: the result of sqrt
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (sqrt1(n, 0));
 }
 
 /**
- * actual_sqrt_recursion - recurses to find the natural
+ * sqrt1 - recurses to find the natural
  * square root of a number
  * @n: number to calculate the sqaure root of
  * @i: iterator
  *
  * Return: the resulting square root
  */
-int actual_sqrt_recursion(int n, int i)
+int sqrt1(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	return (sqrt1(n, i + 1));
 }
+
