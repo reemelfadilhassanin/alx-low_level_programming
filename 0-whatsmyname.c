757d12172bdd32a43eec45c9f6bf38bdb5e014e7
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-09-01 18:22:42 -0700

    my commit

---
+++ 0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main  function to print program
+ * @argc: count of arrgument
+ * @argv: array of pointer to string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
