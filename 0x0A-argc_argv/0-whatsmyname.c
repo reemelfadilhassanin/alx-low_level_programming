5e35896896eace11979a6f46785da98178e86c3d
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-09-01 18:32:17 -0700

    my commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main  function to print program
+ * main -function to print program
  * @argc: count of arrgument
  * @argv: array of pointer to string
  * Return: 0
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
-	return (0);
+        if (argc > 0)
+                printf("%s\n", argv[0]);
+        return (0);
 }
