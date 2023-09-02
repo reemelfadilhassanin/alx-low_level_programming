0b8f8e71d65aa183a30d099cc958f43f4267c51a
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-09-01 19:18:32 -0700

    my commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,18 +8,35 @@
  */
 int main(int argc, char **argv)
 {
-	int x = 0;
-	int y = 0;
 
-	if (argc > 2)
-	{
-		x = atoi(argv[1]);
-		y = atoi(argv[2]);
-		printf("%d\n", x * y);
-	}
-	else
+	 int total;
+
+	if (argc < 2)
 	{
 		printf("Error\n");
+		return (1);
 	}
+
+	total = (int) *argv[1] * (int) *argv[2];
+
+	printf("%d\n", total);
+
 	return (0);
-}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
