beb17da4407d9728ca80f770c6f709529e2ff777
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-09-06 05:11:42 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,39 +6,38 @@
  * @s2: seconed string
  * Return: concat of two string
  */
-
 char *str_concat(char *s1, char *s2)
 {
-int x, y, len1, len2, len;
-char *result;
+	char *ptr;
+	int x, y;
 
-len1 = len2 = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-if (s1 != NULL)
-{
-x = 0;
-while (s1[x++] != '\0')
-len1++;
-}
+	x = y = 0;
+	while (s1[x] != '\0')
+		x++;
+	while (s2[y] != '\0')
+		y++;
+	ptr = malloc(sizeof(char) * (x + y + 1));
 
-if (s2 != NULL)
-{
-x = 0;
-while (s2[x++] != '\0')
-len2++;
-}
+	if (x == NULL)
+		return (NULL);
+	x = y = 0;
+	while (s1[x] != '\0')
+	{
+		ptr[x] = s1[x];
+		x++;
+	}
 
-len = len1 + len2;
-result = (char *)malloc(sizeof(char) * (len + 1));
-if (result == NULL)
-return (NULL);
-
-for (x = 0; x < len1; x++)
-result[x] = s1[x];
-for (y = 0; y < len2; y++, y++)
-result[x] = s2[y];
-result[len] = '\0';
-
-return (result);
+	while (s2[y] != '\0')
+	{
+		ptr[x] = s2[y];
+		x++, y++;
+	}
+	ptr[x] = '\0';
+	return (ptr);
 }
 
