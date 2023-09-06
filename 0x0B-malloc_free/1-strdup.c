ef8992e9542efcbd7abbcf4b8efa15209082a345
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-09-06 04:36:49 -0700

    commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,41 +7,18 @@
  */
 char *_strdup(char *str)
 {
-	char *aaa;
+int i, len;
+char *x;
 
-	int i, r = 0;
+if (str == NULL)
+return (NULL);
+for (len = 0; str[len] != '\0'; len++)
 
-
-
-	if (str == NULL)
-
-		return (NULL);
-
-	i = 0;
-
-	while (str[i] != '\0')
-
-		i++;
-
-
-
-	aaa = malloc(sizeof(char) * (i + 1));
-
-
-
-	if (aaa == NULL)
-
-		return (NULL);
-
-
-
-	for (r = 0; str[r]; r++)
-
-		aaa[r] = str[r];
-
-
-
-	return (aaa);
-
+x = (char *)malloc((len + 1) * sizeof(char));
+if (x == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+x[i] = str[i];
+x[i] = '\0';
+return (x);
 }
-
