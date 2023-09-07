575726091a548f6fa054f83252d1d1cc8d130265
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-09-07 16:03:34 -0700

    concat

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *ptr1;
-	char *old_ptr;
+	char *old;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -32,18 +32,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!ptr1)
 		return (NULL);
 
-	old_ptr = ptr;
+	old = ptr;
 
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			ptr1[i] = old_ptr[i];
+			ptr1[i] = old[i];
 	}
 
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			ptr1[i] = old_ptr[i];
+			ptr1[i] = old[i];
 	}
 
 	free(ptr);
