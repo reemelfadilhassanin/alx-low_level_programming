dba1a798ba5f21422d53734c7303292bdbcacd7a
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-09-11 21:46:24 -0700

    commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,16 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
- * free_dog - free allocated memory 
- * @d: pointer 
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
  */
 void free_dog(dog_t *d)
 {
-
 	if (d)
 	{
-		if (d->name)
-			free(d->name);
-		if (d->owner)
-			free(d->owner);
+		free(d->name);
+		free(d->owner);
 		free(d);
 	}
 }
