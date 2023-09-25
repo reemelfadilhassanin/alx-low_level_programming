8211ad7ab2e94af258681fa82755be615e53dc26
reemelfadilhassanin <justreemjust49@gmail.com>
2023-09-25 12:16:43 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - print a linked lists
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
+
