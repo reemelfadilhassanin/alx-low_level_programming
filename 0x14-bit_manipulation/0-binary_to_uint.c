b0166522a9b745ea82f0f5d3d361a8f7749e5667
reemelfadilhassanin <justreemjust49@gmail.com>
2023-09-28 12:47:57 -0700

    commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,19 @@
  *
  * Return: the converted binary to int
  */
-unsigned int binary_to_uint(const char *b) {
-    unsigned int res = 0;
-    
-    if (b == NULL)
-        return 0;
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int res = 0;
 
+if (b == NULL)
+return (0);
+while (*b)
+{
+if (*b != '0' && *b != '1')
+return (0);
 
-    while (*b)
-    {
-        if (*b != '0' && *b != '1')
-            return 0;
-
-        res = (res << 1) | (*b - '0');
-        b++;
-    }
-
-    return res;
+res = (res << 1) | (*b - '0');
+b++;
+}
+return (res);
 }
