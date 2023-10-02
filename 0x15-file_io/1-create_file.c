4da580bd4d9413b0f2891b5cc5012a7c9686bd2a
reemelfadilhassanin <justreemjust49@gmail.com>
2023-10-02 12:52:00 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,47 +1,34 @@
 #include "main.h"
- /**
-  * _strlen - funtction get the length of string
-  * @s: the string who check
-  * Return: integer length
-  */
-int _strlen(char *s)
-{
-int i = 0;
-if (!s)
-return (0);
-while (*s++)
-i++;
-return (i);
-}
+
 /**
- * create_file - Function that creates a file.
- * @filename: A name of file to be created
- * @text_content: A null terminate string
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-int f;
-ssize_t byte_w, len = _strlen(text_content);
+	int fd, w, len = 0;
 
-if (filename == NULL)
-return (-1);
-f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-if (f == -1)
-{
-return (-1);
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
-if (text_content != NULL)
-{
-byte_w = write(f, text_content,  len);
-close(f);
-if (byte_w == -1)
-{
-return (-1);
-}
-}
-close(f);
-return (1);
-}
+
