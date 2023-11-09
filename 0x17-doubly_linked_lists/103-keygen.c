eb280074af15802c0d8ca5bd9a241739d36c61ca
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-10 02:45:34 +0400

    crackmenod3

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 /**
- * main - Generates and prints passwords for the crackme5 executable.
- * @argc: The number of arguments supplied to the program.
+ * main - Generates and prints passwords
+ * @argc: The number of arguments  program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: Always 0.
