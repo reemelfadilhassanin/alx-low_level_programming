4e8c31256d56f4f0cdd446139cb012a7158b5330
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-10 02:14:07 +0400

    insert nod

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,25 +3,27 @@
 /**
  * insert_dnodeint_at_index - function that inserts a new node at,
  * a given position.
- * @h: pointer to pointer to the h of linked list.
+ * @h: pointer to pointer to head node of linked list.
  * @idx: index of the list where the new  node should be added.
- * @n: value of the new node.
+ * @n: value of the new node in linked list.
  *
  * if it is not possible to add the new node at index idx, do not,
  * add the new node and return NULL.
  *
- * Return:  the address of the new node, or NULL if it failed.
+ * Return:  the address of the new node, or NULL otherwise.
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new;
 	dlistint_t *head;
 	unsigned int i;
+    if (h == NULL)
+		return (NULL);
 
-	new_node = NULL;
-	if (idx == 0)	/* insert node at beginning of list */
-		new_node = add_dnodeint(h, n);
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
 	else
 	{
 		head = *h;
@@ -33,19 +35,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			if (i == idx)
 			{
-				/* insert note at the end of list */
+				
 				if (head->next == NULL)
-					new_node = add_dnodeint_end(h, n);
+					new = add_dnodeint_end(h, n);
 				else
 				{
-					new_node = malloc(sizeof(dlistint_t));
-					if (new_node != NULL)
+					new = malloc(sizeof(dlistint_t));
+                    if (new == NULL)
+		return (NULL);
+					if (new != NULL)
 					{
-						new_node->n = n;
-						new_node->next = head->next;
-						new_node->prev = head;
-						head->next->prev = new_node;
-						head->next = new_node;
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
 					}
 				}
 				break;
@@ -54,5 +58,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			i++;
 		}
 	}
-	return (new_node);
+	return (new);
 }
