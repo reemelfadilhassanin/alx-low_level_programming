67be7b488f5e45a829546bea37f411ef69b941c2
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-10 02:19:15 +0400

    insert nod

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,20 +16,20 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *tmp_node = *h;
+	dlistint_t *new = malloc(sizeof(dlistint_t)), *tmp_node = *h;
 
-	if (new_node)
+	if (new)
 	{
-		new_node->n = n;
+		new->n = n;
 		if (idx == 0)
 		{
 			if (*h)
 			{
-				new_node->next = *h;
-				(*h)->prev = new_node;
+				new->next = *h;
+				(*h)->prev = new;
 			}
-			*h = new_node;
-			return (new_node);
+			*h = new;
+			return (new);
 		}
 		while (i < idx - 1 && tmp_node)
 		{
@@ -39,11 +39,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (tmp_node)
 		{
 			if (tmp_node->next)
-				tmp_node->next->prev = new_node;
-			new_node->next = tmp_node->next;
-			new_node->prev = tmp_node;
-			tmp_node->next = new_node;
-			return (new_node);
+				tmp_node->next->prev = new;
+			new->next = tmp_node->next;
+			new->prev = tmp_node;
+			tmp_node->next = new;
+			return (new);
 		}
 	}
 	return (NULL);
