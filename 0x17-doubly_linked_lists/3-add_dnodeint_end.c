446b3b5a58be127d7874124101aa6365cd3dd26a
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-10 00:05:11 +0400

    add node

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (!head || !new)
-		return (new ? free (new), NULL:NULL);
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 
