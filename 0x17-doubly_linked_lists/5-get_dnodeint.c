5c76a45e8e774a86cc7a2408c3896f3aa484c509
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-10 01:10:21 +0400

    get nod

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function with get nth node
+ * @head: head pointer to head of linked list
+ * @index: nth node to get node
+ *
+ * Description: returns the nth node of a doubly linked list
+ * Return: nth node or NULL otherwise
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    dlistint_t *current = head;
+	while (current != 0  && index != 0)
+	{
+		current = current->next;
+		index--;
+	}
+	
+		return (current);
+	
+}
