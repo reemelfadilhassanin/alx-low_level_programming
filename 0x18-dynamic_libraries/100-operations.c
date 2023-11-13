cd1f69b3d9cb65268c43aacb3696cd9d33362670
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-11-13 23:32:17 +0400

    pythoncall

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -12,22 +12,13 @@ int mul(int a, int b) {
     return a * b;
 }
 
-int div(int a, int b) {
-    if (b = 0) {
-        return  printf("Error: Division by zero\n");
-        return 0;
-    } 
-        
-    return a / b;
+int div(int a, int b) {  
+    return (a / b);
 
 }
 
 int mod(int a, int b) {
-    if (b = 0) {
-        return  printf("Error: Modulo by zero\n");
-        return 0;
-    } 
        
-        return a % b;
+        return (a % b);
     
 }
