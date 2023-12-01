a0e7fd455fafa43bd8e7ec4c904cf17fac1ff44f
“reemelfadilhassanin” <justreemjust49@gmail.com>
2023-12-01 10:07:05 +0400

    add new node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,48 +3,45 @@
 #include <string.h>
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: The hash table
- * @key: The key of the new element
- * @value: The value of the new element
+ * hash_table_set - This function dds an element to the hash table
+ * @ht: The hash table to add or update
+ * @key: The key of the new element not be an empty string
+ * @value: The value (not be an empty string) of the new element
  *
- * Return: 1 on success, 0 on failure
+ * Return: 1 on success, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = 0;
-	hash_node_t *new_hash_node = NULL;
-	hash_node_t *tmp = NULL;
+	unsigned long int idx = 0;
+	hash_node_t *new_hash = NULL;
+	hash_node_t *tmpe = NULL;
 
 	if (!ht || !key || !(*key) || !value)
 		return (0);
 
-	index = key_index((unsigned char *)key, ht->size);
-	tmp = ht->array[index];
+	idx = key_index((unsigned char *)key, ht->size);
+	tmpe = ht->array[idx];
 
-	/* check if key exists */
-	while (tmp && strcmp(tmp->key, key) != 0)
-		tmp = tmp->next;
-
-	/* update value if key already exists */
-	if (tmp)
+	while (tmpe && strcmp(tmpe->key, key) != 0)
+		tmpe = tmpe->next;
+	if (tmpe)
 	{
-		free(tmp->value);
-		tmp->value = strdup(value);
+		free(tmpe->value);
+		tmpe->value = strdup(value);
 		return (1);
 	}
 
-	/* add new node if key not found */
+	/* check if key not found then add new node*/
 
-	new_hash_node = malloc(sizeof(*new_hash_node));
-	if (!new_hash_node)
+	new_hash = malloc(sizeof(*new_hash));
+	if (!new_hash)
 		return (0);
 
-	new_hash_node->key = strdup(key);
-	new_hash_node->value = strdup(value);
+	new_hash->key = strdup(key);
+	new_hash->value = strdup(value);
 
-	new_hash_node->next = ht->array[index];
-	ht->array[index] = new_hash_node;
+	new_hash->next = ht->array[idx];
+	ht->array[idx] = new_hash;
 
 	return (1);
 }
