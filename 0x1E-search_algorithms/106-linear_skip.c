62f84be1edc741bff43ed83e0cb09155353c8f78
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-14 00:12:59 +0400

    akipliat

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -9,38 +9,38 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *last = list;
+	skiplist_t *last = list;
 
-    if (!list)
-        return (NULL);
+	if (!list)
+		return (NULL);
 
-    while (list->n < value)
-    {
-        if (!list->express)
-            break;
-        last = list;
-        list = list->express;
-        printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
-    }
-    if (list->n >= value)
-    {
-        printf("Value found between indexes [%lu] and [%lu]\n",
-               last->index, list->index);
-        list = last;
-    }
-    else
-    {
-        for (last = list; last->next; last = last->next)
-            ;
-        printf("Value found between indexes [%lu] and [%lu]\n",
-               list->index, last->index);
-    }
-    while (list)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
-        if (list->n == value)
-            return (list);
-        list = list->next;
-    }
-    return (NULL);
+	while (list->n < value)
+	{
+		if (!list->express)
+			break;
+		last = list;
+		list = list->express;
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+	}
+	if (list->n >= value)
+	{
+		printf("Value found between indexes [%lu] and [%lu]\n",
+			   last->index, list->index);
+		list = last;
+	}
+	else
+	{
+		for (last = list; last->next; last = last->next)
+			;
+		printf("Value found between indexes [%lu] and [%lu]\n",
+			   list->index, last->index);
+	}
+	while (list)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+	return (NULL);
 }
