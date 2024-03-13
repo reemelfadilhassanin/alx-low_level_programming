79f1d07b13d63f607a87dde6417ff7e830a7b355
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-14 00:01:55 +0400

    linked list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,8 @@
 #define SEARCH_ALGOS_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 void print_array(int *array, size_t s, size_t end);
@@ -11,5 +13,6 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int adva_search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
