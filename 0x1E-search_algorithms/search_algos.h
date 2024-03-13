fe89dd1d00a0af036eff9b68f14798d4d8170863
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-03-14 00:11:56 +0400

    akipliat

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,36 @@
 #include <stdio.h>
 #include <math.h>
 
+/**
+ * struct listint_s - this struct to singly linked
+ *
+ * @n: Integer number
+ * @index: Index of the node
+ * @next: Pointer to next node
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list with skip
+ *
+ * @n: Integer number
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the skiplist
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
 void print_array(int *array, size_t s, size_t end);
 int binary_search(int *array, size_t size, int value);
@@ -14,5 +44,6 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int adva_search(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
